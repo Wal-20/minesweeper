a4e82c11a42bd17aa4f8bf7ee22b46e1de7c1c47
Waleed <waleedabdulhadi4@gmail.com>
2024-01-17 22:12:15 +0300

    added algorithm to solve the game,still needs to be fixed

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const height = 16;
 const board_width = 40 * width;
 const board_height = 40 * height;
 
-const mines_count = 40;
+const mines_count = 50;
 let mines_locations = [];
 let tilesCleared = 0; // click all tiles except the ones containing a bomb
 
@@ -26,7 +26,9 @@ function startGame() {
     document.querySelector('.board').style.height = board_height + 'px';
     document.getElementById('mines-count').innerHTML = mines_count;
     document.getElementById('flag-button').addEventListener('click',toggleFlag);
-
+    document.getElementById('computer-play').addEventListener('click',computerMove);
+    setMines();
+    
     // populate the board
     for(let i = 0; i < height;i++){
         let row = [];
@@ -37,10 +39,11 @@ function startGame() {
             tile.addEventListener('contextmenu',(event) => {
                 if(!gameOver && !board[i][j].classList.contains('tiles-cleared')) {
                     event.preventDefault(); // Disable right-click context menu
-                    tile.innerText === '🚩' ? tile.innerText = '' : tile.innerText = '🚩';
+                    tile.innerText === '❓' ? tile.innerText = '' : tile.innerText = '❓';
                 }})
             document.querySelector('.board').append(tile);
             row.push(tile);
+            tileScores.set(tile.id,0); // used to calculate adjacent bombs later
         }
         board.push(row);
     }
@@ -123,8 +126,8 @@ function revealMines(backgroundColor) {
 
     for (let i = 0; i < mines_count; i++) {
         let mineCoordinates = mines_locations[i].split('-');
-        let row = parseInt(mineCoordinates[0]);
-        let column = parseInt(mineCoordinates[1]);
+        let row = mineCoordinates[0];
+        let column = mineCoordinates[1];
         let tileID = row + '-' + column;
         document.getElementById(tileID).innerHTML = "💣";
         document.getElementById(tileID).style.backgroundColor = backgroundColor;
@@ -144,9 +147,6 @@ function checkMines(row,column) {
     board[row][column].classList.add('tiles-cleared');
     board[row][column].innerText = ''; // incase a flag is placed on the tile
     tilesCleared++;
-    if(tilesCleared === 1) {
-        setMines();
-    }
 
     let minesFound = 0;
     
@@ -165,6 +165,9 @@ function checkMines(row,column) {
     if(minesFound > 0) {
         board[row][column].innerText = minesFound;
         board[row][column].classList.add('x' + minesFound);
+        tileScores.set(row + '-' + column, minesFound);
+        SearchSurrounding(row,column);
+        // Here we want to evaluate adjacent bombs
     }
 
     else {
@@ -179,6 +182,7 @@ function checkMines(row,column) {
         checkMines(row + 1,column + 1);
         checkMines(row + 1,column - 1);
     }
+
 }
 
 
@@ -193,3 +197,180 @@ function checkTile(row,column) {
 
     return 0;
 }   
+
+//---------------------------------------------------------------------------------- COMPUTER MOVE ---------------------------------------------------------------------------------------------------------------------------
+
+let bombsConfirmed = new Set(); // push confirmed tile IDs to this array
+let unsafeTiles = [];   // if surrounding tiles are more than that tile's score, add them here
+let safeTiles = [];
+
+let tileScores = new Map();
+
+// we want to search the surrounding tiles for all cleared tiles, if the score == surrounding uncleared tiles, we can confirm those tiles as mines
+
+function subtractScore(row,column) {
+    if(bombsConfirmed.has(row + '-' + column)) return;
+
+    bombsConfirmed.add(row + '-' + column);
+    // subtract score of surrounding tiles
+    if(row - 1 > -1) {
+        const tileScore = tileScores.get(row - 1 + '-' + column);
+        tileScores.set(row - 1 + '-' + column,tileScore - 1);
+    }
+    
+    if(row + 1 < height) {
+        const tileScore = tileScores.get(row + 1 + '-' + column);
+        tileScores.set(row + 1 + '-' + column,tileScore - 1);
+    }
+
+    if(column - 1 > -1) {
+        const tileScore = tileScores.get(row + '-' + column + 1);
+        tileScores.set(row + '-' + column + 1,tileScore - 1);
+    }
+    
+    if(column + 1 < width) {
+        const tileScore = tileScores.get(row + '-' + column + 1);
+        tileScores.set(row + '-' + column + 1,tileScore - 1);
+    }
+
+    if(column + 1 < width && row - 1 > -1) {
+        const tileScore = tileScores.get(row - 1 + '-' + column + 1);
+        tileScores.set(row - 1 + '-' + column + 1,tileScore - 1);
+    }
+    
+    if(column - 1 > -1 && row - 1 > -1) {
+        const tileScore = tileScores.get(row - 1 + '-' + column - 1);
+        tileScores.set(row - 1 + '-' + column - 1,tileScore - 1);
+    }
+
+    if(column - 1 > -1 && row + 1 > -1) {
+        const tileScore = tileScores.get(row + 1 + '-' + column - 1);
+        tileScores.set(row + 1 + '-' + column - 1,tileScore - 1);
+    }
+
+    if(column + 1 < width && row + 1 < height) {
+        const tileScore = tileScores.get(row + 1 + '-' + column + 1);
+        tileScores.set(row + 1 + '-' + column + 1,tileScore - 1);
+    }
+}
+// when a bomb is confirmed subtract 1 from the score of all surrounding cleared tiles
+
+
+function SearchSurrounding(row,column) {    // PRIORITY FOR SEARCHING IS FOR TILES WITH THE LEAST SCORES
+    
+    let surroundingUncleared = 0;
+    if(row - 1 > -1 && !board[row - 1][column].classList.contains('tiles-cleared') && !bombsConfirmed.has(row - 1 + '-' + column)) {
+        unsafeTiles.push(row - 1 + '-' + column);
+        surroundingUncleared++;
+    }
+    
+    if(row + 1 < height && !board[row + 1][column].classList.contains('tiles-cleared') && !bombsConfirmed.has(row + 1 + '-' + column)) {
+        unsafeTiles.push(row + 1 + '-' + column);
+        surroundingUncleared++;
+    }
+
+    if(column - 1 > -1 && !board[row][column - 1].classList.contains('tiles-cleared') && !bombsConfirmed.has(row + '-' + column - 1)) {
+        unsafeTiles.push(row + '-' + column - 1);
+        surroundingUncleared++;
+    }
+    
+    if(column + 1 < width && !board[row][column + 1].classList.contains('tiles-cleared') && !bombsConfirmed.has(row + '-' + column + 1)) {
+        unsafeTiles.push(row + '-' + column + 1);
+        surroundingUncleared++;
+    }
+    
+    if(column + 1 < width && row - 1 > -1 && !board[row - 1][column + 1].classList.contains('tiles-cleared') && !bombsConfirmed.has(row - 1 + '-' + column + 1)) {
+        unsafeTiles.push(row - 1 + '-' + column + 1);
+        surroundingUncleared++;
+    }
+    
+    if(column - 1 > -1 && row - 1 > -1 && !board[row - 1][column - 1].classList.contains('tiles-cleared') && !bombsConfirmed.has(row - 1 + '-' + column - 1)) {
+        unsafeTiles.push(row - 1 + '-' + column - 1);
+        surroundingUncleared++;
+    }
+
+    if(column - 1 > -1 && row + 1 < height && !board[row + 1][column - 1].classList.contains('tiles-cleared') && !bombsConfirmed.has(row + 1 + '-' + column - 1)) {
+        unsafeTiles.push(row + 1 + '-' + column - 1);
+        surroundingUncleared++;
+    }
+
+    if(column + 1 < width && row + 1 < height && !board[row + 1][column + 1].classList.contains('tiles-cleared') && !bombsConfirmed.has(row + 1 + '-' + column + 1)) {
+        unsafeTiles.push(row + 1 + '-' + column + 1);
+        surroundingUncleared++;
+    }
+    if(surroundingUncleared === tileScores.get(row + '-' + column)) {
+        // grab all surrounding uncleared tiles and add them to bombsConfirmed
+        for(let i = 0; i < surroundingUncleared;i++) {
+            let tileID = unsafeTiles.pop();
+            let row = parseInt(tileID[0]);
+            let column = parseInt(tileID[1]);
+            subtractScore(row,column);
+        }
+    }
+
+    else if(tileScores.get(row + '-' + column) === 0 && surroundingUncleared > 0) { // clear all tiles that are not bombs
+        const surroundingTiles = getSurroundingTiles(row,column);
+        for(let i = 0; i < surroundingTiles.length;i++) {
+            safeTiles.push(surroundingTiles[i]);
+        }
+    }
+
+}
+
+function getSurroundingTiles(row,column) {
+    let surroundingTiles = [];
+    if(row - 1 > -1) {
+        surroundingTiles.push(row - 1 + '-' + column);
+    }
+    
+    if(row + 1 < height) {
+        surroundingTiles.push(row + 1 + '-' + column);
+    }
+
+    if(column - 1 > -1) {
+        surroundingTiles.push(row + '-' + column - 1);
+    }
+    
+    if(column + 1 < width) {
+        surroundingTiles.push(row + '-' + column + 1);
+    }
+
+    if(column + 1 < width && row - 1 > -1) {
+        surroundingTiles.push(row - 1 + '-' + column + 1);
+    }
+    
+    if(column - 1 > -1 && row - 1 > -1) {
+        surroundingTiles.push(row - 1 + '-' + column - 1);
+    }
+
+    if(column - 1 > -1 && row + 1 > -1) {
+        surroundingTiles.push(row + 1 + '-' + column - 1);
+    }
+
+    if(column + 1 < width && row + 1 < height) {
+        surroundingTiles.push(row + 1 + '-' + column + 1);
+    }
+    return surroundingTiles;
+}
+
+function computerMove() {
+    if (safeTiles.length > 0) {
+        let tileID = safeTiles.pop().split('-');
+        let row = parseInt(tileID[0]);
+        let column = parseInt(tileID[1]);
+        checkMines(row, column);
+    } 
+    else {
+        let row = getRandomIndex(height);
+        let column = getRandomIndex(width);
+        while(board[row][column].classList.contains('tiles-cleared')) {
+            row = getRandomIndex(height);
+            column = getRandomIndex(width);
+        }
+        checkMines(row, column);
+    }
+}
+
+function ReverseSortScores(tileScores) {
+    return tileScores.sort((a, b) => b.value - a.value);
+}
